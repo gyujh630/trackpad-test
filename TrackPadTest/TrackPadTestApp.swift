8581dd39c6e844feebd383df31c4a04de7138fff
gyujh630 <rbwjrbxo@ajou.ac.kr>
2023-09-15 16:08:30 +0900

    Initial Commit

---
+++ TrackPadTest/TrackPadTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackPadTestApp.swift
+//  TrackPadTest
+//
+//  Created by 한규정 on 2023/09/15.
+//
+
+import SwiftUI
+
+@main
+struct TrackPadTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

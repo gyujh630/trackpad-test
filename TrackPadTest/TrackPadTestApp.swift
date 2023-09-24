77aeeb859455e9f0321d1bad8dd1e88d28cb381b
gyujh630 <rbwjrbxo@ajou.ac.kr>
2023-09-24 19:24:00 +0900

    Feat: TrackPad touch test

--- TrackPadTest/TrackPadTestApp.swift
+++ TrackPadTest/TrackPadTestApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TrackPadTestApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().frame(minWidth: 900, idealWidth: 900, minHeight: 600, idealHeight: 600)
         }
     }
 }
